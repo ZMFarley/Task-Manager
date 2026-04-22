b87bc6e5cd349d51be2c7b979a73ba13844a1970
ZMFarley <zacharymfarley@gmail.com>
2026-04-21 22:00:16 -0400

    Added more functionality to api.py, began black box and white box testing

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 import sqlite3
 from datetime import date
 from contextlib import asynccontextmanager
+from typing import Annotated
 
 #Lifespan to create database if it doesnt exist
 @asynccontextmanager
@@ -41,7 +42,7 @@ class TaskPatch(BaseModel):
 
 
 # CREATE TASK
-@app.post("/create-task", response_model=TaskGrabber, status_code=201)
+@app.post("/tasks", response_model=TaskGrabber, status_code=201)
 def create_tasks(task: Task):
     #Make Database Connection 
     with sqlite3.connect("tasks.db") as conn:
@@ -56,9 +57,10 @@ def create_tasks(task: Task):
     
     
 # READ TASK
-@app.get("/read-task/{id}", response_model=TaskGrabber)
-def get_tasks(id: int):
-    #Make Databse Connection 
+@app.get("/tasks/{id}", response_model=TaskGrabber)
+#
+def get_tasks(id: Annotated[int, Path(title="Bounds of ID",gt=0,le=9223372036854775807)]):
+    #Make Database Connection 
     with sqlite3.connect("tasks.db") as conn:
         cursor = conn.cursor()
         #Insert Task 
@@ -70,7 +72,7 @@ def get_tasks(id: int):
             raise HTTPException(status_code=404, detail="Task Not Found")           
 
 # UPDATE TASK
-@app.patch("/patch-task/{id}", response_model=TaskGrabber)
+@app.patch("/tasks/{id}", response_model=TaskGrabber)
 def patch_tasks(id: int, task: TaskPatch):
     tasks = task.model_dump(exclude_unset=True)
     query = []
@@ -96,7 +98,7 @@ def patch_tasks(id: int, task: TaskPatch):
         row = cursor.fetchone()
         return {"id": row[0], "due_date": row[1], "task": row[2]}        
 
-@app.put("/put-task/{id}", response_model=TaskGrabber)
+@app.put("/tasks/{id}", response_model=TaskGrabber)
 def put_tasks(id: int, task: Task):
      with sqlite3.connect("tasks.db") as conn:
         cursor = conn.cursor()
@@ -111,7 +113,7 @@ def put_tasks(id: int, task: Task):
         return {"id": row[0], "due_date": row[1], "task": row[2]}             
 
 # DELETE TASK
-@app.delete("/delete-task/{id}", status_code=204)
+@app.delete("/tasks/{id}", status_code=204)
 def delete_tasks(id: int):
      with sqlite3.connect("tasks.db") as conn:
         cursor = conn.cursor()
---
+++ test_blackbox.py
@@ -0,0 +1,249 @@
+from fastapi.testclient import TestClient
+from api import app
+from api import init_db
+import sqlite3
+
+#API Section 
+client = TestClient(app)
+
+def check_existing_table(flag: bool):
+        #Test Setup
+
+        #Delete any currently active tables to allow for fresh testing 
+        with sqlite3.connect("tasks.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("DROP TABLE IF EXISTS TASKS")
+
+        #Reinitalize table for testing if being used to start up testing
+        if flag:
+                init_db()
+def test_black_box_post():
+        #Test Setup
+        check_existing_table(True)
+        #Black Box Tests for POST requests
+        #BVA AND ECP TEST CASES FOR TASK INPUT
+
+        '''
+           BVA/ECP TESTING FOR TASK FIELD
+        '''
+        #BVA/ECP MIN- LENGTH STRING TASK (Empty String)
+        task = {"due_date": "2020-02-02", "task": ""}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'String should have at least 1 character'
+
+        #BVA MIN Length (1) STRING TASK
+        task = {"due_date": "2020-02-02", "task": "A"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 201
+        assert response.json() == {"id": 1, "due_date": "2020-02-02", "task": "A"}
+
+        #BVA/ECP  (MIN+) MIDDLE LENGTH STRING TASK
+        task = {"due_date": "2024-04-02", "task": "PA"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 201
+        assert response.json() == {"id": 2, "due_date": "2024-04-02", "task": "PA"}
+
+        #BVA MAX- LENGTH STRING 254 CHARACTERS
+        task = {"due_date": "1923-12-01", "task": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 201
+        assert response.json() == {"id": 3, "due_date": "1923-12-01", "task": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}
+        
+        #BVA MAX LENGTH STRING 255 CHARACTERS
+        task = {"due_date": "1999-12-01", "task": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 201
+        assert response.json() == {"id": 4, "due_date": "1999-12-01", "task": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}
+        
+        #ECP MAX+ LENGTH STRING TASK 256 CHARACTERS
+        task = {"due_date": "2020-02-02", "task": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'String should have at most 255 characters'
+        
+
+        '''
+        BVA/ECP TESTING FOR DUE DATE FIELD
+        '''
+        #BVA/ECP MIN- DATE 
+        task = {"due_date": "0000-01-01", "task": "fly to the moon"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be a valid date in the format YYYY-MM-DD, year 0 is out of range'
+
+        #BVA/ECP MIN DATE (0001-01-01)
+        task = {"due_date": "0001-01-01", "task": "itch my foot"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 201
+        assert response.json() == {"id": 5, "due_date": "0001-01-01", "task": "itch my foot"}
+
+        #BVA/ECP MIN+ DATE (0002-01-01)
+        task = {"due_date": "0002-01-01", "task": "itch my leg"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 201
+        assert response.json() == {"id": 6, "due_date": "0002-01-01", "task": "itch my leg"}
+        
+        #ECP INCORRECT FORMAT MM/DD/YYYY OR DD/MM/YYYY, BOTH WILL BE TREATED THE SAME
+        task = {"due_date": "04/20/1920", "task": "fly to the clouds"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be a valid date or datetime, invalid character in year'
+
+        #ECP INCORRECT FORMAT MM-DD-YYYY OR DD-MM-YYYY, BOTH WILL BE TREATED THE SAME
+        task = {"due_date": "09-12-1920", "task": "fly to the sky"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be a valid date or datetime, invalid character in year'
+
+        #ECP INVALID DATE, NON EXISTENT DATE 
+        task = {"due_date": "02-30-1920", "task": "fly to the Barber"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be a valid date or datetime, invalid character in year'
+
+        #BVA MAX DATE (9999-12-31)
+        task = {"due_date": "9999-12-31", "task": "take out the trash"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 201
+        assert response.json() == {"id": 7, "due_date": "9999-12-31", "task": "take out the trash"}
+        
+        #BVA/ECP MAX+ DATE 
+        task = {"due_date": "10000-12-31", "task": "take out the trash"}
+        response = client.post("/tasks",json=task)
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be a valid date or datetime, invalid date separator, expected `-`'
+        #Test Completion
+        #Drop test table
+        check_existing_table(False)
+
+def test_black_box_get():
+        #Task set up for testing
+        check_existing_table(True)
+        client.post("/tasks",json={"due_date": "1220-12-31", "task": "Do whatever the Romans do"})
+        client.post("/tasks",json={"due_date": "1492-12-31", "task": "Sail the Ocean Blue"})
+
+        '''
+        BVA/ECP TESTING FOR ID FIELD
+        '''
+
+        #BVA/ECP MIN- ID (0)
+        response = client.get("/tasks/0")
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be greater than 0'
+
+        #BVA/ECP MIN ID (1)
+        response = client.get("/tasks/1")
+        assert response.status_code == 200
+        assert response.json() == {"id": 1, "due_date": "1220-12-31", "task": "Do whatever the Romans do"}
+
+        #BVA/ECP MIN+ ID
+        response = client.get("/tasks/2")
+        assert response.status_code == 200
+        assert response.json() == {"id": 2, "due_date": "1492-12-31", "task": "Sail the Ocean Blue"}
+
+
+        #BVA/ECP MAX- (9223372036854775806)
+        with sqlite3.connect("tasks.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO Tasks(id, due_date, task) VALUES (?, ?, ?)",
+                (9223372036854775806, "2024-01-23", "Run"))
+        response = client.get("/tasks/9223372036854775806")
+        assert response.status_code == 200
+        assert response.json() == {"id": 9223372036854775806, "due_date": "2024-01-23", "task": "Run"}
+
+        #BVA/ECP MAX (9223372036854775807)
+        with sqlite3.connect("tasks.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO Tasks(id, due_date, task) VALUES (?, ?, ?)",
+                (9223372036854775807, "2025-01-24", "Run Faster"))
+        response = client.get("/tasks/9223372036854775807")
+        assert response.status_code == 200
+        assert response.json() == {"id": 9223372036854775807, "due_date": "2025-01-24", "task": "Run Faster"}
+        
+        #BVA/ECP MAX+ (9223372036854775808)
+        response = client.get("/tasks/9223372036854775808")
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be less than or equal to 9223372036854775807'
+
+        #ECP NON EXISTING VALID ID
+        response = client.get("/tasks/3")
+        assert response.status_code == 404
+        assert response.json()["detail"] == 'Task Not Found'
+
+        #ECP NON NUMERIC INPUT
+        response = client.get("/tasks/abc")
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be a valid integer, unable to parse string as an integer'
+
+        #ECP INVALID FLOAT INPUT
+        response = client.get("/tasks/1.5")
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be a valid integer, unable to parse string as an integer'
+
+        check_existing_table(False)
+
+def test_black_box_patch():
+        pass
+def test_black_box_put():
+        pass
+def test_black_box_delete():
+        #Task set up for testing
+        check_existing_table(True)
+        client.post("/tasks",json={"due_date": "1220-12-31", "task": "Do whatever the Romans do"})
+        client.post("/tasks",json={"due_date": "1492-12-31", "task": "Sail the Ocean Blue"})
+
+        '''
+        BVA/ECP TESTING FOR ID FIELD
+        '''
+
+        #BVA/ECP MIN- ID (0)
+        response = client.delete("/tasks/0")
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be greater than 0'
+
+        #BVA/ECP MIN ID (1)
+        response = client.delete("/tasks/1")
+        assert response.status_code == 204
+
+        #BVA/ECP MIN+ ID
+        response = client.delete("/tasks/2")
+        assert response.status_code == 204
+
+        #BVA/ECP MAX- (9223372036854775806)
+        with sqlite3.connect("tasks.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO Tasks(id, due_date, task) VALUES (?, ?, ?)",
+                (9223372036854775806, "2024-01-23", "Run"))
+        response = client.delete("/tasks/9223372036854775806")
+        assert response.status_code == 204
+
+        #BVA/ECP MAX (9223372036854775807)
+        with sqlite3.connect("tasks.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO Tasks(id, due_date, task) VALUES (?, ?, ?)",
+                (9223372036854775807, "2025-01-24", "Run Faster"))
+        response = client.delete("/tasks/9223372036854775807")
+        assert response.status_code == 200
+        
+        #BVA/ECP MAX+ (9223372036854775808)
+        response = client.get("/tasks/9223372036854775808")
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be less than or equal to 9223372036854775807'
+
+        #ECP NON EXISTING VALID ID
+        response = client.get("/tasks/3")
+        assert response.status_code == 404
+        assert response.json()["detail"] == 'Task Not Found'
+
+        #ECP NON NUMERIC INPUT
+        response = client.get("/tasks/abc")
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be a valid integer, unable to parse string as an integer'
+
+        #ECP INVALID FLOAT INPUT
+        response = client.get("/tasks/1.5")
+        assert response.status_code == 422
+        assert response.json()["detail"][0]['msg'] == 'Input should be a valid integer, unable to parse string as an integer'
+
+        check_existing_table(False)
