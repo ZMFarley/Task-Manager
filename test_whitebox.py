c461231d4eb9051764d0617a6b9da15e38f08fce
ZMFarley <zacharymfarley@gmail.com>
2026-04-22 16:27:27 -0400

    finished whitebox testing

--- test_whitebox.py
+++ test_whitebox.py
@@ -1,8 +1,6 @@
 from fastapi.testclient import TestClient
 from api import app
-from api import init_db
 from test_blackbox import check_existing_table
-import sqlite3
 
 #API Section 
 client = TestClient(app)
@@ -59,26 +57,39 @@ def test_white_box_patch():
     assert response.status_code == 404
     assert response.json() == {'detail': 'Task Not Found'}
 
-    
-def test_white_box_put():
-    check_existing_table(True)
-    #ROW EXISTS TRUE BRANCH
-    #ROW DOES NOT EXIST BRANCH 
+    #NO TASKS HAVE BEEN PASSED 
+    response = client.patch("/tasks/1", json={})
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'No Data provided'}
 
-    #ENDPOINT EXECUTION SUCCESS
+def test_white_box_put():
+    #TASK SET UP 
+    check_existing_table(True)
     task = {"due_date": "2020-02-02", "task": "A"}
     response = client.post("/tasks",json=task)
+
+    #ROW EXISTS TRUE BRANCH/ENDPOINT EXECUTION SUCCESS
     task = {"due_date": "2020-02-04", "task": "Abe"}
     response = client.put("/tasks/1",json=task)
     assert response.status_code == 200
     assert response.json() == {"id": 1, "due_date": "2020-02-04", "task": "Abe"}
-    pass
+
+    #ROW DOES NOT EXIST BRANCH 
+    task = {"due_date": "1911-03-21","task": "Abela"}
+    response = client.put("/tasks/2",json=task)
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Task Not Found'}
+
 def test_white_box_delete():
     check_existing_table(True)
-    #ROW EXISTS TRUE BRANCH
-    #ROW DOES NOT EXIST BRANCH 
-    #ENDPOINT EXECUTION SUCCESS
+
+    #ROW EXISTS TRUE BRANCH/ENDPOINT EXECUTION SUCCESS
     task = {"due_date": "2020-02-02", "task": "A"}
     response = client.post("/tasks",json=task)
     response = client.delete("/tasks/1")
     assert response.status_code == 204
+
+    #ROW DOES NOT EXIST BRANCH 
+    response = client.delete("/tasks/2")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Task Not Found'}
