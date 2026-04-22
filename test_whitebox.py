3ccc2364ce51f037540d07a806c6450a6d9b56a6
ZMFarley <zacharymfarley@gmail.com>
2026-04-22 16:02:35 -0400

    added basic whitebox tests

--- test_whitebox.py
+++ test_whitebox.py
@@ -20,29 +20,47 @@ def test_white_box_get():
     #ROW EXISTS TRUE BRANCH
     #ROW DOES NOT EXIST BRANCH 
     #ENDPOINT EXECUTION SUCCESS
-    pass
+    task = {"due_date": "2020-02-02", "task": "A"}
+    response = client.post("/tasks",json=task)
+    response = client.get("/tasks/1")
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "due_date": "2020-02-02", "task": "A"}
+    
 def test_white_box_patch():
     check_existing_table(True)
     #DYNAMIC QUERY CREATION FOR TASK ONLY
     #DYANMIC QUERY CREATION FOR DUE DATE ONLY
     #DYANMIC QUERY CREATION FOR BOTH 
-    #SQL UPDATE EXECUTION
-    #SQL SELECTION EXECUTION
     #ROW EXISTS TRUE BRANCH
     #ROW DOES NOT EXIST BRANCH 
+
     #ENDPOINT EXECUTION SUCCESS
-    pass
+    task = {"due_date": "2020-02-02", "task": "A"}
+    response = client.post("/tasks",json=task)
+    task = {"task": "Abe"}
+    response = client.patch("/tasks/1",json=task)
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "due_date": "2020-02-02", "task": "Abe"}
+    
 def test_white_box_put():
     check_existing_table(True)
-    #SQL UPDATE EXECUTION
-    #SQL SELECTION EXECUTION
+    #ROW EXISTS TRUE BRANCH
+    #ROW DOES NOT EXIST BRANCH 
+
     #ENDPOINT EXECUTION SUCCESS
+    task = {"due_date": "2020-02-02", "task": "A"}
+    response = client.post("/tasks",json=task)
+    task = {"due_date": "2020-02-04", "task": "Abe"}
+    response = client.put("/tasks/1",json=task)
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "due_date": "2020-02-04", "task": "Abe"}
     pass
 def test_white_box_delete():
     check_existing_table(True)
-    #SQL INSERT EXECUTION
-    #SQL SELECTION EXECUTION
     #ROW EXISTS TRUE BRANCH
     #ROW DOES NOT EXIST BRANCH 
     #ENDPOINT EXECUTION SUCCESS
-    pass
+    task = {"due_date": "2020-02-02", "task": "A"}
+    response = client.post("/tasks",json=task)
+    response = client.delete("/tasks/1")
+    assert response.status_code == 204
